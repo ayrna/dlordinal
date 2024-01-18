41d10563f3eea0b594ac8c47e88444e4c2d12931
Victor Vargas <vvargas@uco.es>
2024-01-18 09:21:08 +0100

    unimodal tests

--- dlordinal/losses/tests/test_triangular_loss.py
+++ dlordinal/losses/tests/test_triangular_loss.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from ..triangular_loss import TriangularCrossEntropyLoss
@@ -8,7 +9,7 @@ def test_triangular_loss_creation():
     assert isinstance(loss, TriangularCrossEntropyLoss)
 
 
-def test_triangular_loss_output():
+def test_triangular_loss_basic():
     loss = TriangularCrossEntropyLoss(num_classes=6)
 
     input_data = torch.tensor(
@@ -28,3 +29,124 @@ def test_triangular_loss_output():
 
     # Verifies that the loss is greater than zero
     assert output.item() > 0
+
+
+def test_triangular_loss_exactvalue():
+    loss = TriangularCrossEntropyLoss(num_classes=6)
+
+    input_data = torch.tensor(
+        [
+            [0.8, 0.1, 0.1, 0.0, 0.0, 0.0],
+            [0.1, 0.8, 0.1, 0.0, 0.0, 0.0],
+            [0.0, 0.1, 0.8, 0.1, 0.0, 0.0],
+        ]
+    )
+    target = torch.tensor([0, 1, 2])
+
+    # Compute the loss
+    output = loss(input_data, target)
+
+    # Verifies that the output is a tensor
+    assert isinstance(output, torch.Tensor)
+
+    # Verifies that the loss is greater than zero
+    assert output.item() == pytest.approx(1.25947, rel=1e-3)
+
+
+def test_triangular_loss_relative():
+    loss = TriangularCrossEntropyLoss(num_classes=6)
+
+    input_data = torch.tensor(
+        [
+            [100.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    input_data2 = torch.tensor(
+        [
+            [0.0, 100.0, 0.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    input_data3 = torch.tensor(
+        [
+            [0.0, 0.0, 100.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    target = torch.tensor([0])
+
+    # Compute the loss
+    output = loss(input_data, target)
+    output2 = loss(input_data2, target)
+    output3 = loss(input_data3, target)
+
+    # Verifies that the output is a tensor
+    assert isinstance(output, torch.Tensor)
+
+    assert output3.item() >= output2.item() > output.item()
+
+
+def test_triangular_loss_eta():
+    input_data = torch.tensor(
+        [
+            [0.0, 0.0, 100.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    target = torch.tensor([1])
+
+    last_loss = None
+    for eta in [0.1, 0.3, 0.5, 0.7, 1.0]:
+        loss = TriangularCrossEntropyLoss(num_classes=6, eta=eta)
+
+        # Compute the loss
+        output = loss(input_data, target)
+
+        if last_loss is not None:
+            assert output.item() < last_loss.item()
+
+        last_loss = output
+
+
+def test_triangular_loss_alpha2():
+    input_data = torch.tensor(
+        [
+            [0.0, 0.0, 100.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    target = torch.tensor([1])
+
+    last_loss = None
+    for alpha2 in [0.01, 0.05, 0.1, 0.15, 0.2]:
+        loss = TriangularCrossEntropyLoss(num_classes=6, alpha2=alpha2)
+
+        # Compute the loss
+        output = loss(input_data, target)
+
+        if last_loss is not None:
+            assert output.item() < last_loss.item()
+
+        last_loss = output
+
+
+def test_triangular_loss_weights():
+    weights = torch.tensor([5.0, 2.0, 1.0, 0.5, 0.1, 0.1])
+    loss = TriangularCrossEntropyLoss(num_classes=6, weight=weights)
+
+    input_data = torch.tensor(
+        [
+            [0.0, 0.0, 100.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    target = torch.tensor([0])
+    target2 = torch.tensor([1])
+    target3 = torch.tensor([3])
+
+    loss1 = loss(input_data, target)
+    loss2 = loss(input_data, target2)
+    loss3 = loss(input_data, target3)
+
+    assert loss1.item() > loss2.item() > loss3.item()
