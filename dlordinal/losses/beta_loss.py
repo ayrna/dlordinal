faf7ce427f523d0ac057426ef15f065283957b90
victormvy <victormvy@gmail.com>
2024-04-15 20:10:12 +0200

    changed get_beta_softlabels to output a matrix with all soft labels

--- dlordinal/losses/beta_loss.py
+++ dlordinal/losses/beta_loss.py
@@ -6,116 +6,6 @@ from torch import Tensor
 from ..soft_labelling import get_beta_softlabels
 from .custom_targets_loss import CustomTargetsCrossEntropyLoss
 
-# Params [a,b] for beta distribution
-_beta_params_sets = {
-    "standard": {
-        3: [[1, 4, 1], [4, 4, 1], [4, 1, 1]],
-        4: [[1, 6, 1], [6, 10, 1], [10, 6, 1], [6, 1, 1]],
-        5: [[1, 8, 1], [6, 14, 1], [12, 12, 1], [14, 6, 1], [8, 1, 1]],
-        6: [[1, 10, 1], [7, 20, 1], [15, 20, 1], [20, 15, 1], [20, 7, 1], [10, 1, 1]],
-        7: [
-            [1, 12, 1],
-            [7, 26, 1],
-            [16, 28, 1],
-            [24, 24, 1],
-            [28, 16, 1],
-            [26, 7, 1],
-            [12, 1, 1],
-        ],
-        8: [
-            [1, 14, 1],
-            [7, 31, 1],
-            [17, 37, 1],
-            [27, 35, 1],
-            [35, 27, 1],
-            [37, 17, 1],
-            [31, 7, 1],
-            [14, 1, 1],
-        ],
-        9: [
-            [1, 16, 1],
-            [8, 40, 1],
-            [18, 47, 1],
-            [30, 47, 1],
-            [40, 40, 1],
-            [47, 30, 1],
-            [47, 18, 1],
-            [40, 8, 1],
-            [16, 1, 1],
-        ],
-        10: [
-            [1, 18, 1],
-            [8, 45, 1],
-            [19, 57, 1],
-            [32, 59, 1],
-            [45, 55, 1],
-            [55, 45, 1],
-            [59, 32, 1],
-            [57, 19, 1],
-            [45, 8, 1],
-            [18, 1, 1],
-        ],
-        11: [
-            [1, 21, 1],
-            [8, 51, 1],
-            [20, 68, 1],
-            [34, 73, 1],
-            [48, 69, 1],
-            [60, 60, 1],
-            [69, 48, 1],
-            [73, 34, 1],
-            [68, 20, 1],
-            [51, 8, 1],
-            [21, 1, 1],
-        ],
-        12: [
-            [1, 23, 1],
-            [8, 56, 1],
-            [20, 76, 1],
-            [35, 85, 1],
-            [51, 85, 1],
-            [65, 77, 1],
-            [77, 65, 1],
-            [85, 51, 1],
-            [85, 35, 1],
-            [76, 20, 1],
-            [56, 8, 1],
-            [23, 1, 1],
-        ],
-        13: [
-            [1, 25, 1],
-            [8, 61, 1],
-            [20, 84, 1],
-            [36, 98, 1],
-            [53, 100, 1],
-            [70, 95, 1],
-            [84, 84, 1],
-            [95, 70, 1],
-            [100, 53, 1],
-            [98, 36, 1],
-            [84, 20, 1],
-            [61, 8, 1],
-            [25, 1, 1],
-        ],
-        14: [
-            [1, 27, 1],
-            [2, 17, 1],
-            [5, 23, 1],
-            [9, 27, 1],
-            [13, 28, 1],
-            [18, 28, 1],
-            [23, 27, 1],
-            [27, 23, 1],
-            [28, 18, 1],
-            [28, 13, 1],
-            [27, 9, 1],
-            [23, 5, 1],
-            [17, 2, 1],
-            [27, 1, 1],
-        ],
-    }
-}
-
 
 class BetaCrossEntropyLoss(CustomTargetsCrossEntropyLoss):
     """Beta unimodal regularised cross entropy loss from :footcite:t:`vargas2022unimodal`.
@@ -172,20 +62,8 @@ class BetaCrossEntropyLoss(CustomTargetsCrossEntropyLoss):
         reduction: str = "mean",
         label_smoothing: float = 0.0,
     ):
-        self.params = _beta_params_sets[params_set]
-
         # Precompute class probabilities for each label
-        cls_probs = torch.tensor(
-            [
-                get_beta_softlabels(
-                    num_classes,
-                    self.params[num_classes][i][0],
-                    self.params[num_classes][i][1],
-                    self.params[num_classes][i][2],
-                )
-                for i in range(num_classes)
-            ]
-        ).float()
+        cls_probs = torch.tensor(get_beta_softlabels(num_classes, params_set)).float()
 
         super().__init__(
             cls_probs=cls_probs,
