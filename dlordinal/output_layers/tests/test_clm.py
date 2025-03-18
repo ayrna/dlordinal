b24006d3be3e5c1122eb4bc5dc71a1382a801e06
Francisco Berchez Moreno <fberchez@uco.es>
2025-03-18 13:21:28 +0100

    Updated test_clm.py to work on gpu and cpu

--- dlordinal/output_layers/tests/test_clm.py
+++ dlordinal/output_layers/tests/test_clm.py
@@ -7,53 +7,60 @@ import torch
 from dlordinal.output_layers import CLM
 
 
-def _test_probas(clm):
-    projections = torch.rand(32, 1)
+@pytest.fixture
+def device():
+    return "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def _test_probas(clm, device):
+    projections = torch.rand(32, 1).to(device)
     probas = clm(projections)
-    total_probas = torch.sum(probas, dim=1)
+    total_probas = torch.sum(probas, dim=1).to(device)
     assert torch.allclose(total_probas, torch.ones_like(total_probas))
     assert isinstance(probas, torch.Tensor)
 
     return projections, probas, total_probas
 
 
-def test_clm_creation():
+def test_clm_creation(device):
     num_classes = 3
     link_function = "logit"
     min_distance = 0.0
 
     clm = CLM(
         num_classes=num_classes, link_function=link_function, min_distance=min_distance
-    )
+    ).to(device)
 
     assert isinstance(clm, CLM)
 
 
-def test_clm_probas():
+def test_clm_probas(device):
     num_classes = 5
     link_function = "logit"
     min_distance = 0.0
 
     clm = CLM(
         num_classes=num_classes, link_function=link_function, min_distance=min_distance
-    )
+    ).to(device)
 
-    _test_probas(clm)
+    _test_probas(clm, device)
 
 
-def test_clm_thresholds():
+def test_clm_thresholds(device):
     num_classes = 5
     link_function = "logit"
     min_distance = 0.0
 
     clm = CLM(
         num_classes=num_classes, link_function=link_function, min_distance=min_distance
-    )
+    ).to(device)
 
     thresholds = clm._convert_thresholds(
         clm.thresholds_b_, clm.thresholds_a_, min_distance
     )
-    expected_thresholds = torch.tensor([float(i) for i in range(num_classes - 2 + 1)])
+    expected_thresholds = torch.tensor(
+        [float(i) for i in range(num_classes - 2 + 1)]
+    ).to(device)
 
     assert (
         thresholds.shape[0] == clm.thresholds_b_.shape[0] + clm.thresholds_a_.shape[0]
@@ -61,20 +68,22 @@ def test_clm_thresholds():
 
     assert torch.allclose(thresholds, expected_thresholds)
 
-    _test_probas(clm)
+    _test_probas(clm, device)
 
 
-def test_clm_link_functions():
+def test_clm_link_functions(device):
     for link in ["logit", "probit", "cloglog"]:
         for num_classes in range(3, 12):
-            clm = CLM(num_classes=num_classes, link_function=link, min_distance=0.0)
+            clm = CLM(num_classes=num_classes, link_function=link, min_distance=0.0).to(
+                device
+            )
             assert clm.link_function == link
             assert clm.num_classes == num_classes
 
-            _test_probas(clm)
+            _test_probas(clm, device)
 
 
-def test_clm_all_combinations():
+def test_clm_all_combinations(device):
     for link in ["logit", "probit", "cloglog"]:
         for num_classes in range(3, 12):
             for min_distance in np.linspace(0.0, 0.1, 10):
@@ -82,15 +91,15 @@ def test_clm_all_combinations():
                     num_classes=num_classes,
                     link_function=link,
                     min_distance=min_distance,
-                )
+                ).to(device)
                 assert clm.link_function == link
                 assert clm.num_classes == num_classes
                 assert clm.min_distance == min_distance
 
-                _test_probas(clm)
+                _test_probas(clm, device)
 
 
-def test_clm_clip():
+def test_clm_clip(device):
     input_shape = 12
     num_classes = 6
     link_function = "cloglog"
@@ -101,31 +110,31 @@ def test_clm_clip():
         link_function=link_function,
         min_distance=min_distance,
         clip_warning=True,
-    )
-    input_data = torch.rand(8, input_shape) * 100
+    ).to(device)
+    input_data = torch.rand(8, input_shape).to(device) * 100
     with pytest.warns(Warning, match="Clipping"):
         clm(input_data)
 
     warnings.filterwarnings("error")
     clm(input_data)
-    _test_probas(clm)
+    _test_probas(clm, device)
 
     clm = CLM(
         num_classes=num_classes,
         link_function=link_function,
         min_distance=min_distance,
         clip_warning=False,
-    )
+    ).to(device)
     clm(input_data)
-    _test_probas(clm)
+    _test_probas(clm, device)
 
     clm = CLM(
         num_classes=num_classes,
         link_function=link_function,
         min_distance=min_distance,
         clip_warning=True,
-    )
-    input_data = torch.rand(8, input_shape) * 0.1
+    ).to(device)
+    input_data = torch.rand(8, input_shape).to(device) * 0.1
     clm(input_data)
-    _test_probas(clm)
+    _test_probas(clm, device)
     warnings.resetwarnings()
