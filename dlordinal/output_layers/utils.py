ce16fd24133071a284dc3213e90a1ffac350cabf
RafaAyGar <rafaaylloningeniero@gmail.com>
2025-06-19 08:54:00 +0200

    Replace CLM clipping by stable computation of sigmoid in logit link function

---
+++ dlordinal/output_layers/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def stable_sigmoid(t):
+    """
+    Stable sigmoid function that avoids overflow issues for large values of t.
+    This function is used to compute the sigmoid of a tensor t, handling both positive
+    and negative values in a numerically stable way.
+    """
+    idx = t > 0
+    out = torch.zeros_like(t)
+    out[idx] = 1.0 / (1 + torch.exp(-t[idx]))
+    exp_t = torch.exp(t[~idx])
+    out[~idx] = exp_t / (1.0 + exp_t)
+    return out
