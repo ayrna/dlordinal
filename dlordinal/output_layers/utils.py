ff22ee7242132630aa88cb13b0277ff729fdb0d5
RafaAyGar <rafaaylloningeniero@gmail.com>
2025-06-19 09:14:25 +0200

    Documented stable_sigmoid function

--- dlordinal/output_layers/utils.py
+++ dlordinal/output_layers/utils.py
@@ -1,11 +1,16 @@
 import torch
 
 
-def stable_sigmoid(t):
+def stable_sigmoid(t: torch.Tensor) -> torch.Tensor:
     """
     Stable sigmoid function that avoids overflow issues for large values of t.
     This function is used to compute the sigmoid of a tensor t, handling both positive
     and negative values in a numerically stable way.
+
+    Parameters
+    ----------
+    t : torch.Tensor
+        Input tensor
     """
     idx = t > 0
     out = torch.zeros_like(t)
