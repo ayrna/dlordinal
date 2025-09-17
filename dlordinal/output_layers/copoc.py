8f21bfdf36fc74d47800e9adbf12b1b67d6e1ecd
stefanahaas41 <sthaas1923@gmail.com>
2025-09-17 14:18:01 +0200

    Refactor COPOC class docstring for clarity and formatting

--- dlordinal/output_layers/copoc.py
+++ dlordinal/output_layers/copoc.py
@@ -7,22 +7,21 @@ from torch.nn import Module
 
 class COPOC(Module):
     """Implements the  Conformal Predictions for OC (COPOC) output layer(s) from :footcite:t:`dey2023conformal`,
-    which enforce unimodality in the output probabilities in a non-parametric way."""
+    which enforce unimodality in the output probabilities in a non-parametric way.
+
+    Parameters
+    ----------
+    phi: Callable[[Tensor], Tensor]
+        Non-negative transformation function. Default is absolute value function.
+    psi: Callable[[Tensor], Tensor]
+        Strictly monotonic decreasing bijective function. Default is negative absolute value function.
+    """
 
     def __init__(
         self,
         phi: Callable[[Tensor], Tensor] = lambda x: torch.abs(x),
         psi: Callable[[Tensor], Tensor] = lambda x: -torch.abs(x),
     ) -> None:
-        """
-
-        Parameters
-        ----------
-        phi: Callable[[Tensor], Tensor]
-            Non-negative transformation function. Default is absolute value function.
-        psi: Callable[[Tensor], Tensor]
-            Strictly monotonic decreasing bijective function. Default is negative absolute value function.
-        """
         super().__init__()
         self.phi = phi
         self.psi = psi
