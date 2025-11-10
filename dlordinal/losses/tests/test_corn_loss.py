c2efde39dacfc7914761c9974909002d26824d4f
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2025-11-09 21:03:50 -0500

    update zero loss input
    I _think_ this will fix it - I slightly misunderstood the loss function in my previous commits

--- dlordinal/losses/tests/test_corn_loss.py
+++ dlordinal/losses/tests/test_corn_loss.py
@@ -43,9 +43,9 @@ def test_cornloss_zeroloss(device):
 
     input_data = torch.tensor(
         [
-            [0.4, 0.2],
-            [0.6, 0.4],
-            [0.7, 0.6],
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
         ]
     ).to(device)
 
