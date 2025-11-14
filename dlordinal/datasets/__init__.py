c317ded31a60ce58a0eb1bad5d01958da45f5586
Victor Vargas <vvargas@uco.es>
2025-11-14 09:39:26 +0100

    ENH: Added HCI dataset

--- dlordinal/datasets/__init__.py
+++ dlordinal/datasets/__init__.py
@@ -1,5 +1,11 @@
 from .adience import Adience
 from .featuredataset import FeatureDataset
 from .fgnet import FGNet
+from .hci import HCI
 
-__all__ = ["FeatureDataset", "FGNet", "Adience"]
+__all__ = [
+    "FeatureDataset",
+    "FGNet",
+    "Adience",
+    "HCI",
+]
